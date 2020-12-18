25c4f423bf8c4fcb49080bc2ac4c75280f875a46
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2020-12-18 13:49:15 +0900

    post message
    error - handleFormSubmit

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { postMessage, Message } from '../client';
-import { Botton, Form, Segment, TextArea } from 'semantic-ui-react';
+import { Button, Form, Segment, TextArea } from 'semantic-ui-react';
 
 interface MessageFormProps {
     channelName: string;
@@ -15,5 +15,44 @@ export class MessageForm extends React.Component<MessageFormProps, MessageFormSt
         this.state = {
             body: ' '
         }
+        this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
+    public render() {
+        return (
+            <Segment basic textAlign='center'>
+                <Form onSubmit={this.handleFormSubmit}>
+                    <Form.Field>
+                        <TextArea
+                            autoHeight
+                            placeholder='Write your message'
+                            value={this.state.body}
+                            onChange={this.handleTextAreaChange}/>
+                    </Form.Field>
+                    <Button primary type='submit'>Send</Button>
+                </Form>
+            </Segment>
+        )
+    };
+
+    private handleTextAreaChange(event: React.FormEvent<HTMLTextAreaElement>) {
+        event.preventDefault()
+        this.setState( { body: event.currentTarget.value });
+    }
+
+    private handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const payload = {
+            body: this.state.body,
+        } as Message;
+        postMessage(this.props.channelName, payload)
+        .then(() => {
+            this.setState({ body: ''});
+        })
+        .catch(err => {
+            this.setState({ body: 'error'});
+            console.log(err);
+        });
     }
 }
--- src/containers/Channel.tsx
+++ src/containers/Channel.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { match } from 'react-router-dom';
-import { MessageFeed } from '../components';
+import { MessageForm, MessageFeed } from '../components';
 
 interface ChannelMatch {
     channelName: string;
@@ -17,7 +17,10 @@ export class Channel extends React.Component<ChannelProps, {}>  {
     public render() {
         const { channelName } = this.props.match.params;
         return (
-            <MessageFeed channelName={channelName} />
+            [
+                <MessageFeed key='message-feed' channelName={channelName} />,
+                <MessageForm key='message-form' channelName={channelName} />
+            ]
         );
     }
 }
